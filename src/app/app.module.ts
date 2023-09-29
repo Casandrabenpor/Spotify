f9902211a23ae0205c73affe400b77bcaec1fe46
casandrabenpor <casandra211@gmail.com>
2023-09-29 17:06:49 +0200

    interceptor, bearer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
+import { InjectSessionInterceptor } from '@core/interceptors/inject-session.interceptor';
 
 @NgModule({
   declarations: [
-    // Todo: Declaraciones, componentes, directivas,pipes
+    //TODO: Declaraciones, componentes, directivas, pipes
     AppComponent,
   ],
   imports: [
-    // Solo se importan otros modulos
+    //TODO: Solo se importan otros modules
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
   ],
-  providers: [CookieService],
+  providers: [
+    CookieService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InjectSessionInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
